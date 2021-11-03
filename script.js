808e030777d235f5efd750c7e2c7647dc3629089
klikknpro <klikknpro@gmail.com>
2021-11-03 14:10:37 +0100

    Adding weirdness Level 2.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function init() {}
+let button;
+let body;
+let section;
+
+function init() {
+  button = document.getElementById("randomId");
+  button.addEventListener("click", magic);
+}
+
+function magic() {
+  body = document.querySelector("body");
+  body.classList.toggle("magic-body");
+
+  section = document.querySelector(".welcome");
+  section.classList.toggle("magic-section");
+}
 
 window.addEventListener("load", init);
